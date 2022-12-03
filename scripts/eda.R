d6f7585eb050e4f7750d791aa5ad6aff5e0acc7e
Conor Tompkins <conorotompkins@Conors-MBP.localdomain>
2022-12-03 11:10:33 -0500

    updated file path, cleaned up graphs

--- scripts/eda.R
+++ scripts/eda.R
@@ -9,7 +9,7 @@ options(scipen = 999, digits = 4, use.ragg = TRUE)
 
 theme_set(theme_bw())
 
-energy_df <- list.files("inputs", pattern = ".csv", full.names = TRUE) |> 
+energy_df <- list.files("inputs/duquesne_light", pattern = ".csv", full.names = TRUE) |> 
   map_dfr(~read_csv(.x, skip = 4)) |> 
   clean_names()
 
@@ -26,7 +26,14 @@ energy_df <- energy_df |>
 
 glimpse(energy_df)
 
-compare_year_by_month <- energy_df |> 
+#total timeline
+energy_df |>
+  ggplot(aes(date_time, usage)) +
+  geom_point(alpha = .1, size = .3) +
+  geom_smooth(span = .1)
+
+#compare hourly usage by month
+compare_hour_by_month <- energy_df |> 
   ggplot(aes(start_hour, usage)) +
   geom_line(aes(group = date), alpha = .04) +
   geom_smooth(aes(color = year), se = FALSE, span = .3) +
@@ -34,23 +41,20 @@ compare_year_by_month <- energy_df |>
   labs(x = "Hour",
        y = "kWh")
 
-compare_year_by_month
+compare_hour_by_month
 
-ggsave(filename = "outputs/energy_compare_year_by_month.png",
+ggsave(filename = "outputs/compare_hour_by_month.png",
        width = 20, height = 12)
 
-monthly_df <- energy_df |> 
+#compare day of week by month
+dow_df <- energy_df |> 
   group_by(date, year, month, dow) |> 
   summarize(usage = sum(usage)) |> 
   ungroup()
 
-monthly_df |> 
+dow_df |> 
   ggplot(aes(dow, usage)) +
   geom_boxplot(aes(group = dow)) +
   geom_smooth(aes(color = year, group = year), se = FALSE) +
   facet_wrap(vars(month), scales = "free_y") +
   theme_bw()
-
-energy_df |> 
-  as_tsibble(key = usage, index = date_time) |> 
-  fable::gg_
