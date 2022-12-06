ce76d9e483dbadb3e9e69f7977905e8297a1d019
Conor Tompkins <conorotompkins@Conors-MBP.localdomain>
2022-12-05 21:35:38 -0500

    updated eda charts

--- scripts/eda.R
+++ scripts/eda.R
@@ -9,20 +9,14 @@ options(scipen = 999, digits = 4, use.ragg = TRUE)
 
 theme_set(theme_bw())
 
-energy_df <- list.files("inputs/duquesne_light", pattern = ".csv", full.names = TRUE) |> 
-  map_dfr(~read_csv(.x, skip = 4)) |> 
-  clean_names()
-
-glimpse(energy_df)
-
-energy_df <- energy_df |> 
+energy_df <- read_csv("inputs/clean_data/electricity.csv") |> 
   mutate(date_time = str_c(date, start_time, sep = " "),
          date_time = ymd_hms(date_time),
          year = year(date),
          month = month(date, label = T, abbr = FALSE),
          dow = wday(date, label = T),
          start_hour = hour(start_time)) |> 
-  mutate(year = as.factor(year))
+  filter(!(year == 2018 & month == "September"))
 
 glimpse(energy_df)
 
@@ -30,12 +24,22 @@ glimpse(energy_df)
 energy_df |>
   ggplot(aes(date_time, usage)) +
   geom_point(alpha = .1, size = .3) +
-  geom_smooth(span = .1)
+  geom_smooth(span = .1) +
+  geom_smooth(method = "lm")
+
+energy_df |>
+  ggplot(aes(date_time, usage)) +
+  geom_density2d_filled()
+
+energy_df |>
+  ggplot(aes(x = date_time, y = usage)) +
+  geom_bin_2d(aes(alpha = after_stat(count)))
 
 #compare hourly usage by month
 compare_hour_by_month <- energy_df |> 
+  mutate(year = as.factor(year)) |> 
   ggplot(aes(start_hour, usage)) +
-  geom_line(aes(group = date), alpha = .04) +
+  geom_line(aes(group = date), alpha = .02) +
   geom_smooth(aes(color = year), se = FALSE, span = .3) +
   facet_wrap(vars(month), scales = "free_y") +
   labs(x = "Hour",
@@ -48,6 +52,7 @@ ggsave(filename = "outputs/compare_hour_by_month.png",
 
 #compare day of week by month
 dow_df <- energy_df |> 
+  mutate(year = as.factor(year)) |> 
   group_by(date, year, month, dow) |> 
   summarize(usage = sum(usage)) |> 
   ungroup()
@@ -58,3 +63,10 @@ dow_df |>
   geom_smooth(aes(color = year, group = year), se = FALSE) +
   facet_wrap(vars(month), scales = "free_y") +
   theme_bw()
+
+#peoples gas
+gas_df <- read_csv("inputs/clean_data/gas.csv")
+
+gas_df |> 
+  ggplot(aes(bill_date, mcf)) +
+  geom_line()
